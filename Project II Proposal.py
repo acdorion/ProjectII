1a2b39c9b0465f8a3e86d26b57d75cb22f385784
acdorion <82840093+acdorion@users.noreply.github.com>
2021-08-27 23:03:48 -0400

    Update Project II Proposal.py

--- Project II Proposal.py
+++ Project II Proposal.py
@@ -2,7 +2,7 @@ Project II Proposal.py
 Project II
 Matthew Yurich, Madhusudan Tamrakar, Allison Dorion
 
-Objective: Compare marriage rates and divorce rates across all 50 states.
+Objective: Compare marriage rates and divorce rates across all 50 states throughout the last two decades.
 
 Data sources: https://www.cdc.gov/nchs/nvss/marriage-divorce.htm — excel sheets in “Resources” folder
 
